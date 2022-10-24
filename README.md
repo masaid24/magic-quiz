e129c02cfe474cd252b993d63140b9aeb3aab777
masaid24 <mahdisaid24@gmail.com>
2022-10-24 15:52:39 +0100

    git init

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my first react project.
+I've inspired the idea from a react course made by the great "the net ninja".
+It's a simple memory game in which you need to find each two cards that look similar.
---
+++ src/components/SingleCard.js
@@ -0,0 +1,25 @@
+import './SingleCard.css'
+
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card)
+        }
+        handleChoice(card)
+    }
+
+  return (
+        <div className='card'>
+            <div className={flipped ? "flipped" : ""}>
+              <img src={card.src} className='front' alt='card front' />
+              <img 
+                className='back' 
+                src='/img/cover.png' 
+                onClick={handleClick} 
+                alt='card back' 
+              />
+            </div>
+        </div>
+  )
+}
