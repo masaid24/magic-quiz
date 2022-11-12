5ee48c8a17c59bf7ad09d9f4ec672140f68f1dc3
masaid24 <mahdisaid24@gmail.com>
2022-11-12 09:49:12 +0100

    initial commit

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,5 +1,23 @@
 import './SingleCard.css'
 
+import ring from '../img/ring-1.png'
+import cover from '../img/cover.png'
+import potion from '../img/potion-1.png'
+import shield from '../img/shield-1.png'
+import sword from '../img/sword-1.png'
+import scroll from '../img/scroll-1.png'
+import helmet from '../img/helmet-1.png'
+
+const Hash = {
+  "cover" : cover,
+  "ring" : ring,
+  "potion" :potion,
+  "shield" : shield,
+  "sword" : sword,
+  "scroll" : scroll,
+  "helmet" : helmet
+}
+
 export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
     const handleClick = () => {
@@ -12,10 +30,10 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
   return (
         <div className='card'>
             <div className={flipped ? "flipped" : ""}>
-              <img src={card.src} className='front' alt='card front' />
+              <img src={Hash[card.src]} className='front' alt='card front' />
               <img 
                 className='back' 
-                src='/img/cover.png' 
+                src={require('../img/cover.png' )}
                 onClick={handleClick} 
                 alt='card back' 
               />
